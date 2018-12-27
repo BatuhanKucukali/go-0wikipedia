b940bd4afbd99b06a53ebbd837a7b7387cabc85e
batuhankucukali <mail@batuhankucukali.com>
2018-12-27 10:40:17 +0300

    first commit

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+    return {redirectUrl: details.url.replace("wikipedia", "0wikipedia")}
+}, {urls: ["*://*.wikipedia.org/*"]}, ["blocking"]);
