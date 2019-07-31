b51f8d58bb9d1fc98425e11226477f2a3d582ebb
batuhan.kucukali <batuhan.kucukali@trendyol.com>
2019-07-31 22:23:05 +0300

    Changed redirect url because 0wikipedia will be closed.

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    return {redirectUrl: details.url.replace("wikipedia", "0wikipedia")}
+    return {redirectUrl: details.url.replace("wikipedia", "2wikipedia")}
 }, {urls: ["*://*.wikipedia.org/*"]}, ["blocking"]);
